ecfa2a12d94d10650651181946ee92f53045a8c8
cristiannngrey <cristian.hamoay@gmail.com>
2018-05-17 17:10:30 +0800

    wa nako kasabot

--- includes/log.php
+++ includes/log.php
@@ -10,53 +10,16 @@
     $email = $_POST['email'];
     
     if (!empty($firstname) && !empty($lastname) && !empty($agency_address) && !empty($contact_num) && !empty($email) ){ 
-    $sql = "INSERT INTO registration (firstname, lastname, agency_address, contact_num, email)
-    VALUES ('$firstname', '$lastname', '$agency_address', '$contact_num', '$email')";
-    $result = $conn->query($sql);
+        $sql = "INSERT INTO registration (firstname, lastname, agency_address, contact_num, email)
+        VALUES ('$firstname', '$lastname', '$agency_address', '$contact_num', '$email')";
 
-    $sql1 = "SELECT id FROM registration 
-    WHERE firstname = '$firstname', lastname = '$lastname', agency_address = '$agency_address', 
-    contact_num = '$contact_num', email = '$email'";
-    $Qr_code_val = $conn->query($sql1); 
-
-    echo "QR code is ".$Qr_code_val;
-
-    } else {
-                echo '<p class="regnotify"><i>All fields are required!</i></p>';
-            }
-
-    
-/*
-    if  (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['agency_address']) && isset($_POST['contact_num']) && isset($_POST['email'])){
-
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $agency_address = $_POST['agency_address'];
-        $contact_num = $_POST['contact_num'];
-        $email = $_POST['email'];
-    
-
-
-            if (!empty($firstname) && !empty($lastname) && !empty($agency_address) && !empty($contact_num) && !empty($email) ){ 
-                     
-                $query = "INSERT INTO `users` VALUES (
-                    '$firstname',
-                    '$lastname',
-                    '$agency_address',
-                    '$contact_num',
-                    '$email');"
-
-                    $query_run = conn->query($query);
-
-                if ($query_run = mysql_query($query)){
-                    echo '<p class="regnotify"><i>You\'re Successfully Registered!</i></p>';
-                } else {
-                    echo '<p class="regnotify"><i>Sorry we can\'t register you this time. Please Try Again!</i></p>';
-                }  
-            } else {
-                echo '<p class="regnotify"><i>All fields are required!</i></p>';
-            }
+        if ($conn->query($sql) === TRUE) {            
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
-*/
-
+    }
+    else {
+            header("Location: ../register_1.php");        
+    }
 ?>
---
+++ register_1.php
@@ -0,0 +1,41 @@
+<?php
+	include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <script>
+        alert("All fields are required"); 
+    </script> 
+	<div class="row">
+	<div class="center" style="background-color: #aaa;">
+		<h2>Register</h2>
+		<p>Please fill in this form to create an account.</p>
+		<hr>
+		<form action="includes/log.php" method="POST">
+				<div class="container">
+					<label for="Name"><b>Name</b></label>
+					<input type="text" name="firstname" placeholder ="Firstname">
+					<input type="text" name="lastname" placeholder ="Lastname"><br>
+					<label for="agency_address"><b>Agency/Address</b></label>
+					<input type="text" name="agency_address" placeholder ="Agency/Address"><br>
+					<label for="contact_num"><b>Contact No.</b></label>
+					<input type="text" name="contact_num" placeholder ="Contact No."><br>
+					<label for="email"><b>Email</b></label>
+					<input type="text" name="email" placeholder ="Email Address"><br>
+					<button type="submit">OK</button>
+				</div>
+			</form>
+		</div>
+			
+	</div>
+	<div class="footer">
+		cRistian
+	</div>
+
+</body>
+</html>
