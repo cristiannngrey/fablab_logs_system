c2a06e61aa1aa003bb5991b62d7e099342584cac
inreambonanza <inreambonanza@gmail.com>
2018-05-17 12:42:07 +0800

    fixed registration form validation

--- includes/log.php
+++ includes/log.php
@@ -2,13 +2,14 @@
 
     include '../dbh.php';
 
-/*
+
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $agency_address = $_POST['agency_address'];
     $contact_num = $_POST['contact_num'];
     $email = $_POST['email'];
-
+    
+    if (!empty($firstname) && !empty($lastname) && !empty($agency_address) && !empty($contact_num) && !empty($email) ){ 
     $sql = "INSERT INTO registration (firstname, lastname, agency_address, contact_num, email)
     VALUES ('$firstname', '$lastname', '$agency_address', '$contact_num', '$email')";
     $result = $conn->query($sql);
@@ -16,9 +17,16 @@
     $sql1 = "SELECT id FROM registration 
     WHERE firstname = '$firstname', lastname = '$lastname', agency_address = '$agency_address', 
     contact_num = '$contact_num', email = '$email'";
-    $Qr_code_val = $conn->query($sql1); */
-    
+    $Qr_code_val = $conn->query($sql1); 
 
+    echo "QR code is ".$Qr_code_val;
+
+    } else {
+                echo '<p class="regnotify"><i>All fields are required!</i></p>';
+            }
+
+    
+/*
     if  (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['agency_address']) && isset($_POST['contact_num']) && isset($_POST['email'])){
 
         $firstname = $_POST['firstname'];
@@ -31,14 +39,14 @@
 
             if (!empty($firstname) && !empty($lastname) && !empty($agency_address) && !empty($contact_num) && !empty($email) ){ 
                      
-                $query = "INSERT INTO `users` VALUES ('','".
-                    mysql_real_escape_string($firstname)."','".
-                    mysql_real_escape_string($lastname)."','".
-                    mysql_real_escape_string($agency_address)."','".
-                    mysql_real_escape_string($contact_num)."','".
-                    mysql_real_escape_string($email)."')";
+                $query = "INSERT INTO `users` VALUES (
+                    '$firstname',
+                    '$lastname',
+                    '$agency_address',
+                    '$contact_num',
+                    '$email');"
 
-                    $query_run = mysql_query($query);
+                    $query_run = conn->query($query);
 
                 if ($query_run = mysql_query($query)){
                     echo '<p class="regnotify"><i>You\'re Successfully Registered!</i></p>';
@@ -49,6 +57,6 @@
                 echo '<p class="regnotify"><i>All fields are required!</i></p>';
             }
         }
-
+*/
 
 ?>
