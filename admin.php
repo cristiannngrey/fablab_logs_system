d714ae007949c89f0e1d8fb1ff425e992c07bf6f
inreambonanza <inreambonanza@gmail.com>
2018-05-22 13:19:03 +0800

    added new design

--- admin.php
+++ admin.php
@@ -7,17 +7,17 @@
         <center>
             <div class="row">
                 <div class="column3">
-                    <a href="v_view.php"><img src="includes/visit.png" alt="Visitor" height="200"></a><br/>
+                    <a href="v_view.php"><img src="includes/a_visit.png" alt="Visitor" height="200"></a><br/>
                     View Visitors Log<br/><br/>
                 </div>
 
                 <div class="column3">
-                    <a href="u_view.php"><img src="includes/user.png" alt="User" height="200"></a><br/>
+                    <a href="u_view.php"><img src="includes/a_user.png" alt="User" height="200"></a><br/>
                     View Users Log<br/><br/>
                 </div>
 
                 <div class="column3">
-                    <a href="c_view.php"><img src="includes/client.png" alt="CLient" height="200"></a><br/>
+                    <a href="c_view.php"><img src="includes/a_client.png" alt="CLient" height="200"></a><br/>
                     View Clients Log<br/>
                 </div>
             </div>
