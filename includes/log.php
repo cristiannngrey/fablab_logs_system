28ea7ab59c1e5926ee32bc171b0df2aa50daa7b3
cristiannngrey <cristian.hamoay@gmail.com>
2018-05-25 17:27:03 +0800

    tried to modify log.php but failed

--- includes/log.php
+++ includes/log.php
@@ -1,5 +1,4 @@
 <?php
-
     include '../dbh.php';
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
@@ -10,17 +9,13 @@
     if (!empty($firstname) && !empty($lastname) && !empty($agency_address) && !empty($contact_num) && !empty($email) ){ 
         $sql = "INSERT INTO registration (firstname, lastname, agency_address, contact_num, email)
         VALUES ('$firstname', '$lastname', '$agency_address', '$contact_num', '$email')";
-
         if ($conn->query($sql) === TRUE) {            
             $last_id = $conn->insert_id;
-
             $pad_length = 6;
             $pad_char = 0;
             $str_type = 'd';
-
             $format = "%{$pad_char}{$pad_length}{$str_type}";
             $id_str = sprintf($format, $last_id	);
-
             //enhance this code
             //add interactive notification
             echo "New record created successfully. <br>
